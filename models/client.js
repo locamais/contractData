5d000500d24fa03807fdf559d333369a20b685d6
locmais <locmais.locadorajk@gmail.com>
2023-11-13 08:50:18 -0300

    first commit

---
+++ models/client.js
@@ -0,0 +1,28 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../db.js')
+
+const Client = sequelize.define('Client', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    adress: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    cpf: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    tel: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    }
+  })
+
+
+module.exports = Client
---
+++ models/product.js
@@ -0,0 +1,33 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../db.js')
+
+const Product = sequelize.define('Product', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dayPrice: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    weekPrice: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    fortnightPrice: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    monthPrice: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+  })
+
+// sequelize.sync({force:true})
+
+module.exports = Product
